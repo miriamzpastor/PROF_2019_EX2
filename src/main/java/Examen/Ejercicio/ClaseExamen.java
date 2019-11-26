cab0962921d4ef3fa953a1bdea549359683f1eda
miriamzpastor <miriam.zpastor@alumnos.upm.es>
2019-11-26 16:23:17 +0100

    Examen

---
+++ src/main/java/Examen/Ejercicio/ClaseExamen.java
@@ -0,0 +1,33 @@
+package Examen.Ejercicio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClaseExamen {
+	public static List<Integer> primos (int n)throws Exception{
+		//Creo la lista que devolveré
+		List <Integer> solucion = new ArrayList<Integer>();
+		// Array de números impares
+		int[] numeros ={1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71};
+		//números negativos
+		if (n<0) {
+			throw new Exception("EntradaInvalida");
+		}
+		//número primo
+		for (int i = 0; i<numeros.length;i++) {
+			if(n==numeros[i]) {
+				throw new Exception("EntradaInvalida");
+			}
+		 }
+		//casos válidos
+		for (int i = 0; i<numeros.length;i++) {
+			if(n%numeros[i]==0) {
+				solucion.add(numeros[i]);
+				n = n/numeros[i];
+			}
+		 }
+		
+		return solucion;
+	}
+
+}
---
+++ src/test/java/Examen/Ejercicio/ClaseExamenTest.java
@@ -0,0 +1,45 @@
+package Examen.Ejercicio;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ClaseExamenTest {
+	
+	public ClaseExamen prueba = new ClaseExamen();
+	
+	@Test(expected = RuntimeException.class)
+	public void negativo() throws Exception{
+		int ejemplo = -1;
+		prueba.primos(ejemplo);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void primotrece() throws Exception{
+		int ejemplo = 13;
+		prueba.primos(ejemplo);
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void validoseis() throws Exception{
+		int ejemplo = 6;
+		List<Integer> lista = new ArrayList<Integer>();
+		lista.add(1);
+		lista.add(2);
+		lista.add(3);
+		Assert.assertEquals(lista, prueba.primos(ejemplo));
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void validodoscinco() throws Exception{
+		int ejemplo = 25;
+		List<Integer> lista = new ArrayList<Integer>();
+		lista.add(1);
+		lista.add(5);
+		Assert.assertEquals(lista, prueba.primos(ejemplo));
+	}
+	
+}
